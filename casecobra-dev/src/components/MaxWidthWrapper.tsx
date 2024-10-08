96724c21d4d31533d1c8f35ce3a9037e86002422
renyanng <ngrenyan@gmail.com>
2024-10-08 22:31:55 +0800

    Created Hero Page and navbar

---
+++ casecobra-dev/src/components/MaxWidthWrapper.tsx
@@ -0,0 +1,17 @@
+import {cn} from "@/lib/utils"
+import {ReactNode} from "react"
+
+// react component that wraps its children in a div with a max width
+const MaxWidthWrapper = ({className, children}: 
+    {
+        className?: string;
+        children: ReactNode;
+    }
+) => {
+    // return a div with a max width of 1400px, centered horizontally, and some padding
+    return <div className={cn('h-full mx-auto w-full max-w-screen-xl px-2.5 md:px-20', className)}>
+        {children}
+    </div>
+}
+
+export default MaxWidthWrapper;
