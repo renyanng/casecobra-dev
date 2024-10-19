39a4973e9cf5a723f8fe274f95ae18f1f414a69a
renyanng <ngrenyan@gmail.com>
2024-10-19 23:29:43 +0800

    Added step.tsx file

---
+++ casecobra-dev/src/components/ui/Steps.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import { usePathname } from "next/navigation"
+
+const 
+
+const Steps = () => {
+    const pathname = usePathname()
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default Steps
